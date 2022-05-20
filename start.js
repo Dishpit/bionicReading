15afbb353e56a57c9a89106c2fe2d2acf774bee5
Zebulun McNeill <zeb.mcn@gmail.com>
2022-05-20 12:30:03 -0500

    fin

--- start.js
+++ start.js
@@ -10,10 +10,15 @@ for (let i = 0; i < paragraphs.length; i++) {
     const words = this.innerText.split(' ');
     // loop through the words
     for (let j = 0; j < words.length; j++) {
-      // if the word is longer than 3 characters
-      if (words[j].length > 3) {
-        // make the word red
-        words[j] = '<span style="color: red">' + words[j] + '</span>';
+      // if the word has an even length
+      if (words[j].length % 2 === 0) {
+        // make the first half of the word bold
+        words[j] = `<strong>${words[j].substring(0, words[j].length / 2)}</strong>${words[j].substring( words[j].length / 2, words[j].length)}`;
+      }
+      // if the word has an odd length
+      else {
+        // make the first half of the word bold, rounded up
+        words[j] = `<strong>${words[j].substring(0, Math.ceil(words[j].length / 2))}</strong>${words[j].substring( Math.ceil(words[j].length / 2), words[j].length)}`;
       }
     }
     // join the words back into a string
