30361e2b04d7bc8f748bac23d73005e5d12f891a
Zebulun McNeill <zeb.mcn@gmail.com>
2022-05-20 12:13:37 -0500

    setting up basic logic

--- start.js
+++ start.js
@@ -2,19 +2,21 @@
 const paragraphs = document.querySelectorAll('p');
 
 // loop through the paragraphs
+
 for (let i = 0; i < paragraphs.length; i++) {
-  // select the current paragraph
-  let p = paragraphs[i];
-  // create an array of words in the selected paragraph
-  let words = p.innerText.split(' ');
-  // loop through the words
-  for (let j = 0; j < words.length; j++) {
-    // select the current word
-    let word = words[j];
-    // if the word is 1 character long
-    if (word.length > 1) {
-      // add a class to the word
-      word.style.fontWeight = 'bold';
+  // add an event listener to each paragraph
+  paragraphs[i].addEventListener('click', function () {
+    // split the text of the paragraph into an array of words
+    const words = this.innerText.split(' ');
+    // loop through the words
+    for (let j = 0; j < words.length; j++) {
+      // if the word is longer than 3 characters
+      if (words[j].length > 3) {
+        // make the word red
+        words[j] = '<span style="color: red">' + words[j] + '</span>';
+      }
     }
-  }
+    // join the words back into a string
+    this.innerHTML = words.join(' ');
+  });
 }
